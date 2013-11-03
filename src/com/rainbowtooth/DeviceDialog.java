f035f1c7bd41a9aa9b67680596a3ac78eafa19c2
Khalil Fazal <khalil.fazal.0@gmail.com>
2013-11-03 12:01:20 -0500

    ADDED: Dialog to choose which bluetooth device to connect to

---
+++ src/com/rainbowtooth/DeviceDialog.java
@@ -0,0 +1,41 @@
+package com.rainbowtooth;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+/**
+ * Shows a dialog where the user chooses a bluetooth device.
+ * 
+ * @author Khalil Fazal
+ */
+public class DeviceDialog extends DialogFragment {
+
+    /**
+     * @see android.app.DialogFragment#onCreateDialog(android.os.Bundle)
+     */
+    @Override
+    public Dialog onCreateDialog(final Bundle savedInstanceState) {
+        final Bundle bundle = this.getArguments();
+        final AlertDialog.Builder alert = new AlertDialog.Builder(this.getActivity()).setTitle(R.string.chooserTitle);
+
+        if (bundle.getBoolean("empty")) {
+            alert.setItems(R.array.noDevices, null);
+        } else {
+            final SimpleMap devices = (SimpleMap) bundle.getParcelable("devices");
+
+            alert.setItems(devices.getKeys(), new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(final DialogInterface dialog, final int which) {
+                    final BluetoothBinder binder = (BluetoothBinder) DeviceDialog.this.getActivity();
+                    binder.bind(devices.valueAt(which));
+                }
+            });
+        }
+
+        return alert.show();
+    }
+}
